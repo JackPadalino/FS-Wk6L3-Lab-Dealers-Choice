3b32a209a6e801ab38af7492b3a23de614c18b07
JackPadalino <pada0867@gmail.com>
2022-10-03 23:07:00 -0400

    updated db table relationships

--- db/index.js
+++ db/index.js
@@ -3,6 +3,12 @@ const Assignment = require('./assignment');
 const Classroom = require('./classroom');
 const Student = require('./student');
 
+Assignment.belongsToMany(Student,{through:'student|assignment'});
+Student.belongsToMany(Assignment,{through:'student|assignment'});
+
+Student.belongsToMany(Classroom,{through:'student|classroom'});
+Classroom.belongsToMany(Student,{through:'student|classroom'});
+
 module.exports = {
     db,
     Assignment,
