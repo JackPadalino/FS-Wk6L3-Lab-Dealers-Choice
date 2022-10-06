0e3522dc55da4cdf99311a720fd76d5f6284aace
JackPadalino <pada0867@gmail.com>
2022-10-05 20:34:36 -0400

    added new routes to assignment model

--- api/assignment.js
+++ api/assignment.js
@@ -39,6 +39,48 @@ router.get('/:id',async(req,res,next)=>{
     }catch(error){
         next(error)
     };
-})
+});
+
+// POST /assignments/create - creates a new assignment
+router.post('/',async(req,res,next)=>{
+    try{
+        const assignment = await Assignment.create({
+            name:req.body.name,
+            points:req.body.points
+        });
+        res.status(201).send(`New assignment "${assignment.name}" created!`);
+    }catch(error){
+        next(error)
+    };
+});
+
+// PUT /assignments/assignmentId - change the name of an assignment
+router.put('/:id',async(req,res,next)=>{
+    try{
+        const assignment = await Assignment.findByPk(req.params.id);
+        await assignment.update({
+            name:req.body.name
+        });
+        await assignment.save();
+        res.status(201).send('Assignment name changed successfully!');
+    }catch(error){
+        next(error)
+    };
+});
+
+// DELETE /assignments/:id - delete an assignment
+router.delete('/:id',async(req,res,next)=>{
+    try{
+        const assignment = await Assignment.findByPk(req.params.id);
+        if(!assignment){
+            res.status(404).send('Assignment not found!');
+        }else{
+            await assignment.destroy();
+            res.status(201).send('Assignment deleted.');
+        }
+    }catch(error){
+        next(error)
+    };
+});
 
 module.exports = router;
